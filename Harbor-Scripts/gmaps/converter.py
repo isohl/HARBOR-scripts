dc63409433baad866d2615f30568d871c6fa0e28
Sheyne <me@sheyne.com>
2011-07-27 15:45:47 -0600

    commit

---
+++ Harbor-Scripts/gmaps/converter.py
@@ -0,0 +1,19 @@
+import math
+def tile_info(lat, lon, zoom)
+	rad_lat = lat / 180 * math.pi
+	rad_lon = lon / 180 * math.pi
+
+	merc_x = rad_lon
+	merc_y = math.log( math.tan(rad_lat) + 1/ math.cos(rad_lat) )
+
+	cart_x = merc_x + math.pi
+	cart_y = math.pi - merc_y
+
+	px0 = cart_x * 256 / (2 * math.pi )
+	py0 = cart_y * 256 / (2 * math.pi )
+	tile_x = px0 * (2 ** zoom) / 256
+	tile_y = py0 * (2 ** zoom) / 256
+	int(tile_x),int(tile_y),zoom
+
+def createURL(lat, lon, zoom):
+	return "http://mt0.google.com/vt/lyrs=m@158000000&hl=en&x=%d&y=%d&z=%d&s=Ga" % tile_info(lat,lon,zoom) 
---
+++ Harbor-Scripts/gmaps/printer.py
@@ -0,0 +1,2 @@
+import converter
+print converter.createURL(40.301000,-105.150167, 10)
